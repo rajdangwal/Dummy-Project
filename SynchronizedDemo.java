5a3852ec0eb97e360bf85995b0e76a15001fe650
draj888 <31967338+draj888@users.noreply.github.com>
2018-08-19 23:52:12 +0530

    Created the project

---
+++ SynchronizedDemo.java
@@ -0,0 +1,61 @@
+
+class Display
+{
+	public void wish(String name)// declaring synchronized here is inefficient
+	{
+		;;;;;; //1 lakh lines of code
+		synchronized(this)//entire for loop is synchronized
+		{
+			for(int i=0;i<1000;i++)
+			{
+				// if we use synchronized here we will get regular output but not all at once
+				
+				System.out.print("Good Morning: ");
+				try
+				{
+					Thread.sleep(5);
+
+				}
+				catch(InterruptedException e)
+				{
+
+				}
+				System.out.print(name+"\n");
+			}
+		}
+		
+		;;;;; //1 lakh lines of code
+	}
+}
+
+class MyThread extends Thread
+{
+	Display d;
+	String name;
+
+	MyThread(Display d, String name)
+	{
+		this.d = d;
+		this.name = name;
+
+	}
+
+	public void run()
+	{
+		d.wish(name);
+	}
+
+}
+
+public class SynchronizedDemo
+{
+	public static void main(String [] args)
+	{
+		Display d = new Display();
+		MyThread t1 = new MyThread(d,"Raju");
+		MyThread t2 = new MyThread(d,"Pawan");
+
+		t1.start();
+		t2.start();
+	}
+}
